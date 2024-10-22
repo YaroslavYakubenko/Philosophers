0ad5834b6e6752672b0edd41c23e766937198265
Yaroslav <yaroslav@Yaroslavs-Laptop.local>
2024-10-22 17:24:49 +0200

    fixed logic

--- srcs/philo.c
+++ srcs/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yyakuben <yyakuben@student.42.fr>          +#+  +:+       +#+        */
+/*   By: yaroslav <yaroslav@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 22:33:29 by yyakuben          #+#    #+#             */
-/*   Updated: 2024/10/21 19:59:35 by yyakuben         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:15:17 by yaroslav         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,12 +59,9 @@ void	eat(t_philo *philo)
 	if (philo->sim->number_of_times_each_philosopher_must_eat != -1
 		&& philo->meals_eaten
 		>= philo->sim->number_of_times_each_philosopher_must_eat)
-	{
-		pthread_mutex_lock(&philo->print_mutex);
-		printf("%ld %d is done eating\n", get_current_time()
-			- philo->sim->start_time, philo->id);
-		pthread_mutex_unlock(&philo->print_mutex);
-	}
+		philo->sim->sim_terminated = 1;
+	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(philo->right_fork);
 	usleep(philo->sim->time_to_eat * 1000);
 }
 
@@ -96,8 +93,6 @@ void	*philo_routine(void *arg)
 		if (!take_forks(philo))
 			continue ;
 		eat(philo);
-		pthread_mutex_unlock(philo->right_fork);
-		pthread_mutex_unlock(philo->left_fork);
 		sleep_philo(philo);
 	}
 	return (NULL);
